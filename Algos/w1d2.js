d81c632303c65f45288a4b2a21d553641c998340
Caleb Koehn <calebkoehn99@outlook.com>
2021-12-15 16:37:28 -0600

    images

---
+++ Algos/w1d2.js
@@ -0,0 +1,14 @@
+// //problem 1
+// var a =25;
+// a = a-13;
+// console.log(a/2);
+
+// a="hello";
+// console.log(a+"hello");
+
+// for(var i=0; i <10; i++){
+//     console.log(i);
+//     i=i+3;
+// }
+// console.log("outside the loop" + i)
+
