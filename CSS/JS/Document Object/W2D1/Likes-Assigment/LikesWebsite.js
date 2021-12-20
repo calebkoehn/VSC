0295402ba6377023d1321cc9fa4f7f68fb5ff329
Caleb Koehn <calebkoehn99@outlook.com>
2021-12-20 14:35:48 -0600

    .

---
+++ CSS/JS/Document Object/W2D1/Likes-Assigment/LikesWebsite.js
@@ -0,0 +1,16 @@
+var count = 9;
+var countElement = document.querySelector("#likes1");
+
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)";
+}
+
+// var count2 = 12;
+// var countElement2 = document.querySelector("#likes2");
+
+
+// function add2() {
+//     count++;
+//     countElement2.innerText = count2 + " like(s)";
+// }
