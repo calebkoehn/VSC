79a330708c1adc7f91cd27cfd97649f8eb432a9f
Caleb Koehn <calebkoehn99@outlook.com>
2021-12-23 09:56:31 -0600

    Algos

---
+++ Algos/W2D4.js
@@ -0,0 +1,30 @@
+// for(var i =0; i < 3; i++){
+//     for(var j =0; j < 3; j++){
+//         console.log("hey there!");
+//     }
+// }
+
+// var arr2d = [
+//     [2,5,8],
+//     [3,6,1],
+//     [5,7,7]
+// ];
+
+// // console.log(arr2d[1][2]);
+
+// for(var i =0; i < arr2d.length; i++){
+//     for( var j = 0; j < arr2d[i].length; j++)
+//     console.log(arr2d[i][j]);
+// }
+
+
+
+function flatten(arr2d) {
+    var flat = [];
+    
+    return flat;
+}
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
+
