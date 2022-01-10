94f6277070bfff72f739037a94da1aeb78923b43
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-10 16:30:37 -0600

    Removed comments for less confusion

--- Python/fundamentals/fundamentals/functions_intermediate1.py
+++ Python/fundamentals/fundamentals/functions_intermediate1.py
@@ -1,14 +1,25 @@
-# x = [ [5,2,3], [15,8,9] ] 
-# students = [
-#     {'first_name':  'Michael', 'last_name' : 'Bryant'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Andres', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 30} ]
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
 
+x[1] [0] = 15
+print(x)
+
+students[0] ['last_name'] = "Bryant"
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z)
 
 
 students = [
@@ -16,15 +27,24 @@ students = [
         {'first_name' : 'John', 'last_name' : 'Rosales'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-iterateDictionary(students) 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+]
 
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key,val in list[i].items():
+            output += f" {key} - {val},"
+    print(output)
+
+
+def iterate_dictionary2(key_name,list):
+    for i in range(0, len(list)):
+        
+        for key,val in list[i].items():
+            if key == key_name:
+                print(val)
+iterate_dictionary2('first_name',students)
+iterate_dictionary2('last_name',students)
 
 
 
@@ -36,23 +56,13 @@ dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-printInfo(dojo)
-# output:
-7 LOCATIONS
-San Jose
-Seattle
-Dallas
-Chicago
-Tulsa
-DC
-Burbank
-    
-8 INSTRUCTORS
-Michael
-Amy
-Eduardo
-Josh
-Graham
-Patrick
-Minh
-Devon
+
+def print_info(dict):
+    for key,val in dict.items():
+        print("--------------")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+
+
+print_info(dojo)
