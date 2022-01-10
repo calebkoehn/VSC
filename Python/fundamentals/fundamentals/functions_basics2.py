559b84658dceb47a069bfb1be170dac06083c31f
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-10 15:36:35 -0600

    Final Update

--- Python/fundamentals/fundamentals/functions_basics2.py
+++ Python/fundamentals/fundamentals/functions_basics2.py
@@ -20,3 +20,31 @@ def first_plus_length(list):
 print(first_plus_length([1,2,3,4,5]))
 
 
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for x in range(0,len(list)):
+        if list[x] > list[1]:
+            output.append(list[x])
+    print(len(output))
+    return output
+
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
+
+
+def length_and_value(size,value):
+    output = []
+    for x in range(0,size):
+        output.append(value)
+    return output
+
+
+
+
+
+
+print(length_and_value(4,7))
+print(length_and_value(6,2))
