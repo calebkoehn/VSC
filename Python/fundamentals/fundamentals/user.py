4e1f78b44762b22f5a256c2eae9566a3112d5ee2
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-10 19:19:55 -0600

    Updated to add chaining

--- Python/fundamentals/fundamentals/user.py
+++ Python/fundamentals/fundamentals/user.py
@@ -6,9 +6,11 @@ class User:
 
     def make_deposit(self, amount):
         self.amount += amount
+        return self
 
     def make_withdrawal(self,amount):
         self.amount -= amount
+        return self
 
     def display_account_balance(self):
         print(f"User: {self.name}, Balance: {self.amount}")
@@ -18,22 +20,10 @@ caleb = User("Caleb")
 john = User("John Doe")
 sam = User("Sam smith")
 
-caleb.make_deposit(100)
-caleb.make_deposit(100)
-caleb.make_deposit(100)
-caleb.make_withdrawal(50)
-caleb.display_account_balance()
+caleb.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(50).display_account_balance()
 
 
-john.make_deposit(500)
-john.make_deposit(750)
-john.make_withdrawal(125)
-john.make_withdrawal(50)
-john.display_account_balance()
+john.make_deposit(500).make_deposit(750).make_withdrawal(125).make_withdrawal(50).display_account_balance()
 
 
-sam.make_deposit(5000)
-sam.make_withdrawal(350)
-sam.make_withdrawal(1230)
-sam.make_withdrawal(50)
-sam.display_account_balance()
+sam.make_deposit(5000).make_withdrawal(350).make_withdrawal(1230).make_withdrawal(50).display_account_balance()
