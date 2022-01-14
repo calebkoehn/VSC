c77c4aace9200cd3b99f2b83a3ec944dded70eb0
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-13 21:57:50 -0600

    adjusted bootstrap

--- Python/flask/fundamentals/HTML Table/server.py
+++ Python/flask/fundamentals/HTML Table/server.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def render_users():
+def render_data():
     users = [
     {'first_name' : 'Michael', 'last_name' : 'Choi'},
     {'first_name' : 'John', 'last_name' : 'Supsupin'},
     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
     {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-return render_template("index.html")
-
+    return render_template("index.html",users=users)
 
 
 
