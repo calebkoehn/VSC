bd946b5c4abaabc4e286de7106c88fd4335ba1d7
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-14 13:43:28 -0600

    .

--- Python/flask/fundamentals/form_test/server.py
+++ Python/flask/fundamentals/form_test/server.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
 # our index route will handle rendering our form
 @app.route('/')
 def index():
     return render_template("index.html")
-if __name__ == "__main__":
-    app.run(debug=True)
 
-from flask import Flask, render_template, request, redirect # added request
-            
+
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/')	 
+    
+# adding this method
+@app.route('/')
+def show_user():
+    return render_template("index.html")
 
-    return redirect('/')
 
+if __name__ == "__main__":
+    app.run(debug=True)
