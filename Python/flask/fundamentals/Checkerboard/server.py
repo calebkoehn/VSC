8e664df8eb395faeb5cddc9c07a29df3a13f3e1f
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-13 20:55:09 -0600

    Cleaned up code

--- Python/flask/fundamentals/Checkerboard/server.py
+++ Python/flask/fundamentals/Checkerboard/server.py
@@ -5,10 +5,16 @@ print(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",row=8,col=8,color_one='red',color_two='black')
 
 
+@app.route('/<int:x>')
+def row(x):
+    return render_template("index.html",row=x,col=4,color_one='red',color_two='black')
 
+@app.route('/<int:x>/<int:y>')
+def row_col(x,y):
+    return render_template("index.html",row=x,col=y,color_one='red',color_two='black')
 
 
 
