f7d410cf9e89737275928394ce5b7602269726a0
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-14 20:26:04 -0600

    Fixed success page.

--- Python/flask/fundamentals/Dojo_Survey/server.py
+++ Python/flask/fundamentals/Dojo_Survey/server.py
@@ -5,29 +5,22 @@ app.secret_key = 'secret key'
 
 @app.route('/')
 def index():
-    if "name" not in session:
-        name = "Default"
-    else:
-        name = session["name"]
-
-    return render_template('index.html', name=name)
+    return render_template('index.html')
 
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    print("Got Post Info")
-    print(request.form)
-    print(request.form["name"])
-    print(request.form["city"])
-    print(request.form["language"])
-    print(request.form["Comment"])
+@app.route('/survey', methods=['POST'])
+def survey():
 
     session["name"] = request.form['name']
     session["city"] = request.form['city']
     session["language"] = request.form['language']
     session["Comment"] = request.form['Comment']
 
-    return redirect('/')
+    return redirect('/success')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 
