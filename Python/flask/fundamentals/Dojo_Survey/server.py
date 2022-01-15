0ee4183d4623e996b4b10ccb70c55873554a7f75
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-14 19:58:19 -0600

    Dojo Survey

--- Python/flask/fundamentals/Dojo_Survey/server.py
+++ Python/flask/fundamentals/Dojo_Survey/server.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template, redirect, session
-
+from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
-app.secret_key = 'shhhh its a secret'
+
+app.secret_key = 'secret key'
+
+@app.route('/')
+def index():
+    if "name" not in session:
+        name = "Default"
+    else:
+        name = session["name"]
+
+    return render_template('index.html', name=name)
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    print(request.form["name"])
+    print(request.form["city"])
+    print(request.form["language"])
+    print(request.form["Comment"])
+
+    session["name"] = request.form['name']
+    session["city"] = request.form['city']
+    session["language"] = request.form['language']
+    session["Comment"] = request.form['Comment']
+
+    return redirect('/')
 
 
 
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
