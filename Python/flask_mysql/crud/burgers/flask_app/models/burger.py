688aded84fc831bcc751d8cad01facd199ade898
Caleb Koehn <calebkoehn99@outlook.com>
2022-01-22 10:39:20 -0600

    Many to Many relationship code updates

--- Python/flask_mysql/crud/burgers/flask_app/models/burger.py
+++ Python/flask_mysql/crud/burgers/flask_app/models/burger.py
@@ -1,14 +1,35 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
+from flask_app.models import topping
+burger.toppings.append(topping.Topping(topping_data))
 class Burger:
-    def __init__( self , db_data ):
-        self.id = db_data['id']
-        self.name = db_data['name']
-        self.bun = db_data['bun']
-        self.meat = db_data['meat']
-        self.calories = db_data['calories']
-        self.created_at = db_data['created_at']
-        self.updated-at = db_data['updated']
+    def __init__( self , data ):
+        self.id = data['id']
+        self.name = data['name']
+        self.bun = data['bun']
+        self.meat = data['meat']
+        self.calories = data['calories']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated']
+        # We now create a list so that later we can add in all the topping objects that relate to a burger.
+        self.toppings = []
+    # This method will retrieve the burger with all the toppings that are associated with the burger.
+    @classmethod
+    def get_burger_with_toppings( cls , data ):
+        query = "SELECT * FROM burgers LEFT JOIN add_ons ON add_ons.burger_id = burgers.id LEFT JOIN toppings ON add_ons.topping_id = toppings.id WHERE burgers.id = %(id)s;"
+        results = connectToMySQL('burgers').query_db( query , data )
+        # results will be a list of topping objects with the burger attached to each row. 
+        burger = cls( results[0] )
+        for row_from_db in results:
+            # Now we parse the topping data to make instances of toppings and add them into our list.
+            topping_data = {
+                "id" : row_from_db["toppings.id"],
+                "topping_name" : row_from_db["topping_name"],
+                "created_at" : row_from_db["toppings.created_at"],
+                "updated_at" : row_from_db["toppings.updated_at"]
+            }
+            burger.toppings.append( topping.Topping( topping_data ) )
+        return burger
     @classmethod
     def save( cls , data ):
         query = "INSERT INTO restaurants ( name , bun, meat, calories, restaurant_id, created_at , updated_at ) VALUES (%(name)s, %(bun)s, %(meat)s, %(calories)s, %(restaurant_id)s,NOW(),NOW());"
