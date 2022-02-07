512bcdb715bfe9b70cb0761cf6b07b4fbca86f4f
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-06 22:44:20 -0600

    fundamentals1

---
+++ C#/Practice/Fundamentals1/Fundamentals1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 256)
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            }
+            for (int x = 1; x <= 100; x++)  
+            {  
+                if (x % 3 == 0 || x % 5 == 0)  
+                {  
+                    Console.WriteLine(x);  
+                }  
+            
+            } 
+            for (int j = 1; j <= 100; j++)  
+            {  
+                if (j % 3 == 0 && j % 5 == 0)  
+                {  
+                    Console.WriteLine("FizzBuzz");  
+                }  
+                else if (j % 3 == 0)  
+                {  
+                Console.WriteLine("Fizz");  
+                }  
+                else if (j % 5 == 0)  
+                {  
+                Console.WriteLine("Buzz");  
+                }   
+            } 
+        }
+    }
+}
+
