1c46c5a497dcb0ec4d00e5c7392fa3aa832f29f9
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-08 08:53:41 -0600

    Human

--- C#/C#_OOP/Human/human.cs
+++ C#/C#_OOP/Human/human.cs
@@ -11,9 +11,17 @@ class Human
         public int Strength;
         public int Intelligence;
         public int Dexterity;
-        private int health;
+        private int health
         {
             get { return health;}
+        }
+        public Human(string Name, int Strength, int Intelligence, int Dexterity, int health)
+        {
+            Name = "";
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
         }
         public int Attack(Human target)
         {
