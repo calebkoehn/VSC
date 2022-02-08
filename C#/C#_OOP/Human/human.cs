76d62ad91a1392affcf1781e99bcca17f30ceb4a
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-08 08:38:01 -0600

    OOP

---
+++ C#/C#_OOP/Human/human.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Human
+
+{
+
+class Human
+{
+    // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        {
+            get { return health;}
+        }
+        public int Attack(Human target)
+        {
+        }
+    }
+}
+
