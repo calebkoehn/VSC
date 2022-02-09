f0c2e41868c600e9628d51d807f42ea7b84d63cf
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-09 17:45:47 -0600

    SLL

---
+++ C#/Practice/Singly_Linked_List/SllNode.cs
@@ -0,0 +1,16 @@
+namespace Singly_Linked_List
+
+{
+
+    public class SllNode
+    {
+        public int Value;
+        public SllNode Next;
+        public SllNode(int value)
+        {
+            // your code here
+        }
+    }
+
+
+}
