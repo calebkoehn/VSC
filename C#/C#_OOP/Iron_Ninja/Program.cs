0a5386f139b0266bcd1311fc677e02a577748db1
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-09 12:40:48 -0600

    Iron Ninja

--- C#/C#_OOP/Iron_Ninja/Program.cs
+++ C#/C#_OOP/Iron_Ninja/Program.cs
@@ -6,7 +6,29 @@ namespace Iron_Ninja
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Buffet b = new Buffet();
+            SpiceHound sh = new SpiceHound();
+            SweetTooth st = new SweetTooth();
+
+            while(!sh.IsFull)
+                sh.Consume(b.Serve());
+
+            while(!st.IsFull)
+                st.Consume(b.Serve());
+
+            Ninja winner;
+            string title;
+            if(sh.ConsumptionHistory.Count > st.ConsumptionHistory.Count)
+            {
+                winner = sh;
+                title = "Spice Hound";
+            }
+            else
+            {
+                winner = st;
+                title = "Sweet Tooth";
+            }
+            Console.WriteLine($"{title} is crowned the winner! Consuming {winner.ConsumptionHistory.Count} items!");
         }
     }
 }
