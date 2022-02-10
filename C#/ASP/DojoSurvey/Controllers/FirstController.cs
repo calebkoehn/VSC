fd55eff0e12bbea657f74320bcf31a732029d054
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-10 17:23:43 -0600

    Dojo Survey

---
+++ C#/ASP/DojoSurvey/Controllers/FirstController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace DojoSurvey.Controllers     //be sure to use your own project's namespace!
+{
+    public class FirstController : Controller   //remember inheritance??
+    {
+        public static List<string> SurveyResults = new List<string>();
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+        [HttpPost("firstpost")]
+        public IActionResult Firstpost(string results)
+        {
+            SurveyResults.Add(results);
+            return RedirectToAction("Result");
+        }
+        [HttpGet]       //type of request
+        [Route("result")]     //associated route string (exclude the leading /)
+        public ViewResult Result()
+        {
+            return View("Result");
+        }
+    }
+}
