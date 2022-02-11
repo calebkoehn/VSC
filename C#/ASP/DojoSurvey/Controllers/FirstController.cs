7b3d52786fdce6957cf064e67515aecc31087478
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-11 13:46:24 -0600

    Dojo Survey

--- C#/ASP/DojoSurvey/Controllers/FirstController.cs
+++ C#/ASP/DojoSurvey/Controllers/FirstController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using DojoSurvey.Models;
 namespace DojoSurvey.Controllers     //be sure to use your own project's namespace!
 {
     public class FirstController : Controller   //remember inheritance??
     {
-        public static List<string> SurveyResults = new List<string>();
+        public static List<Survey> SurveyResults = new List<Survey>();
         //for each route this controller is to handle:
         [HttpGet]       //type of request
         [Route("")]     //associated route string (exclude the leading /)
@@ -12,20 +13,28 @@ namespace DojoSurvey.Controllers     //be sure to use your own project's namespa
         {
             return View("Index");
         }
-        [HttpPost("firstpost")]
-        public IActionResult firstpost(string name,string dojolocation, string language, string text)
+        //[HttpPost("firstpost")]
+        // public IActionResult firstpost(string name,string dojolocation, string language, string text)
+        // {
+        //     SurveyResults.Add(name);
+        //     SurveyResults.Add(dojolocation);
+        //     SurveyResults.Add(language);
+        //     SurveyResults.Add(text);
+        //     return RedirectToAction("Result");
+        // }
+
+        [HttpPost("addSurvey")]
+        public IActionResult addSurvey(Survey newSurvey)
         {
-            SurveyResults.Add(name);
-            SurveyResults.Add(dojolocation);
-            SurveyResults.Add(language);
-            SurveyResults.Add(text);
+            SurveyResults.Add(newSurvey);
             return RedirectToAction("Result");
         }
+
         [HttpGet]       //type of request
         [Route("Result")]     //associated route string (exclude the leading /)
-        public ViewResult Result()
+        public IActionResult Result()
         {
-            ViewBag.results = SurveyResults;
+            ViewBag.SurveyResults = SurveyResults;
             return View("Result");
         }
     }
---
+++ C#/ASP/DojoSurvey/Models/Survey.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DojoSurvey.Models 
+
+{
+
+    public class Survey
+    {
+        public string name {get;set;}
+        public string dojolocation {get;set;}
+        public string language {get;set;}
+        public string text {get;set;}
+
+    }
+
+}
