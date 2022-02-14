39d1fd8753f1bc4d8047ec9c75a36b6801f876a7
Caleb Koehn <calebkoehn99@outlook.com>
2022-02-14 16:30:28 -0600

    Random Passcode

--- C#/ASP/RandomPasscode/Controllers/HomeController.cs
+++ C#/ASP/RandomPasscode/Controllers/HomeController.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace RandomPasscode.Controllers
 {
     public class HomeController : Controller
+
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -20,14 +22,39 @@ namespace RandomPasscode.Controllers
 
         public IActionResult Index()
         {
+            if(HttpContext.Session.GetString("CreateRandomPassword") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 0);
+            }
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            ViewBag.CreateRandomPassword = HttpContext.Session.GetString("CreateRandomPassword");
             return View();
         }
 
-        [HttpGet("password")]
-        public IActionResult passwordGenerator(string generator)
+        [HttpGet("generate")]
+        // Default size of random password is 14  
+
+        public IActionResult CreateRandomPassword()
         {
-            Random rand = new Random();
-            
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            int size = 14;
+
+            // Select one random character at a time from the string  
+            // and create an array of chars  
+            char[] chars = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            string results = new string(chars);
+            Console.WriteLine(results);
+            HttpContext.Session.SetString("CreateRandomPassword", results);
+            int? amount = HttpContext.Session.GetInt32("Count");
+            Console.WriteLine(amount);
+            HttpContext.Session.SetInt32("Count", (int)++amount);
+            // return new string(chars);
             return RedirectToAction("Index");
         }
 
