4f4dc380bf1d514c852026d8ea5e4f0d7b8a5f82
Caleb Koehn <calebkoehn99@outlook.com>
2022-03-08 14:31:17 -0600

    Form

---
+++ MERN/React/more_forms/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import FormData from './components/FormSubmit';
+
+function App() {
+      const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  return (
+    <div className="App">
+      <Form inputs={state} setInputs={setState} />
+      <FormData data={state} />
+    </div>
+  );
+}
+
+export default App;
--- MERN/React/more_forms/src/components/Form.js
+++ MERN/React/more_forms/src/components/Form.js
@@ -18,26 +18,37 @@ const Form = (props) => {
                 <div className="form-group">
                     <label htmlFor="firstName">First Name: </label> 
                     <input type="text" className="form-control" onChange={onChangeHandler} name="firstName" />
+                    <p>
+                        {inputs.firstName.length > 0 && inputs.firstName.length < 2? "First Name must be at least 2 characters.": ""}
+                    </p>
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Last Name: </label> 
                     <input type="text" className="form-control" onChange={onChangeHandler} name="lastName" />
-                    
+                    <p>
+                        {inputs.lastName.length > 0 && inputs.lastName.length < 2? "Last Name must be at least 2 characters.": ""}
+                    </p>
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email: </label> 
                     <input type="email" className="form-control" onChange={onChangeHandler} name="email"/>
-                
+                    <p>
+                        {inputs.email.length > 0 && inputs.email.length < 2? "Email must be at least 2 characters.": ""}
+                    </p>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password: </label> 
                     <input type="password" className="form-control" onChange={onChangeHandler} name="password"/>
-                    
+                    <p>
+                        {inputs.password.length > 0 && inputs.password.length < 8? "Password must be at least 8 characters.": ""}
+                    </p>
                 </div>
                 <div className="form-group">
                     <label htmlFor="confirmPassword">Confirm Password: </label> 
                     <input type="password" className="form-control" onChange={onChangeHandler} name="confirmPassword"/>
-                    
+                    <p>
+                        {inputs.password.length !== inputs.confirmPassword.length? "Passwords must match.": ""}
+                    </p>
                 </div>
             </div>
         </div>
