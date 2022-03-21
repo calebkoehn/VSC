158e75b0638678b8c12bd439f8526efb46230613
Caleb Koehn <calebkoehn99@outlook.com>
2022-03-21 14:37:31 -0500

    Product Manager

---
+++ MERN/FullStackMERN/ProductManager/server/controllers/product.controller.js
@@ -0,0 +1,14 @@
+const { Product } = require('../models/product.model');
+
+// The method below is new
+module.exports.createProduct = (req, res) => {
+    const { title, price, description } = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
+
---
+++ MERN/FullStackMERN/ProductManager/server/routes/product.routes.js
@@ -0,0 +1,6 @@
+const ProductController = require('../controllers/product.controller');
+module.exports = function(app){
+
+    app.post('/api/product', ProductController.createProduct);
+}
+
