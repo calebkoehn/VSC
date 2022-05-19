97281ccbfbd23f601e8ab70e64e3d51829ffd7df
Caleb Koehn <calebkoehn99@outlook.com>
2022-05-19 11:05:21 -0500

    binary search

---
+++ Coding Interview Practice/May19/binarysearch.js
@@ -0,0 +1,18 @@
+var search = function(nums, target) {
+    let start = 0
+    let end = nums.length -1
+    let mid = Math.floor((start + end ) / 2)
+
+    while (start <= end){
+        if (nums[mid] === target){
+            return mid
+        } else if (nums[mid] > target){
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+        mid = Math.floor((start + end) / 2)
+    }
+    return "-1"
+    
+};
